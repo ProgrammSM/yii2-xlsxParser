11980f6eda897117279058f4000890db4eb56f64
Maksim Sukhomlinov <suhomlinov_mp@ekb.sima-land.ru>
2017-06-16 17:27:45 +0500

    Исправляем в соответствии с задачей

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -24,33 +24,33 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
-    /**
-     * Вычисление стоимости работ из загружаемого xlsx файла
-     *
-     * @return string
-     */
-    public function actionCalc()
-    {
-        /**
-         * Модель экшена
-         *
-         * @var CalcForm
-         */
-        $model = new CalcForm();
-
-        if (Yii::$app->request->isPost) {
-            // Загружаем файл
-            $model->file = UploadedFile::getInstance($model, 'file');
-            if ($path = $model->upload()) {
-                // Загружаем конфигурацию из базы данных в виде массива
-                $config = $model->getConfiguration();
-                // Компонентом xlsxParser производим парсинг и вычисление
-                $model->data = Yii::$app->xlsxParser->parse($path, $config,
-                    500.0);
-
-                return $this->render('table', ['model' => $model]);
-            }
-        }
-        return $this->render('calc', ['model' => $model]);
-    }
+//    /**
+//     * Вычисление стоимости работ из загружаемого xlsx файла
+//     *
+//     * @return string
+//     */
+//    public function actionCalc()
+//    {
+//        /**
+//         * Модель экшена
+//         *
+//         * @var CalcForm
+//         */
+//        $model = new CalcForm();
+//
+//        if (Yii::$app->request->isPost) {
+//            // Загружаем файл
+//            $model->file = UploadedFile::getInstance($model, 'file');
+//            if ($path = $model->upload()) {
+//                // Загружаем конфигурацию из базы данных в виде массива
+//                $config = $model->getConfiguration();
+//                // Компонентом xlsxParser производим парсинг и вычисление
+//                $model->data = Yii::$app->xlsxParser->parse($path, $config,
+//                    500.0);
+//
+//                return $this->render('table', ['model' => $model]);
+//            }
+//        }
+//        return $this->render('calc', ['model' => $model]);
+//    }
 }
