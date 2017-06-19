e7e148381eee431623d2fd42a156cf0fa59f87de
Maksim Sukhomlinov <suhomlinov_mp@ekb.sima-land.ru>
2017-06-19 11:47:50 +0500

    Чистим код, исправляем опечатки
    Исправляем опечатку в SiteController и удаляем ненужные комментарии из SiteController и IndexCest

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,10 +2,7 @@
 
 namespace app\controllers;
 
-//use app\models\CalcForm;
-use Yii;
 use yii\web\Controller;
-//use yii\web\UploadedFile;
 
 /**
  * SiteController класс для главной страницы и вычисления
@@ -15,7 +12,7 @@ use yii\web\Controller;
 class SiteController extends Controller
 {
     /**
-     * Отображаем главную сраницу.
+     * Отображаем главную страницу.
      *
      * @return string
      */
@@ -23,34 +20,4 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
-
-//    /**
-//     * Вычисление стоимости работ из загружаемого xlsx файла
-//     *
-//     * @return string
-//     */
-//    public function actionCalc()
-//    {
-//        /**
-//         * Модель экшена
-//         *
-//         * @var CalcForm
-//         */
-//        $model = new CalcForm();
-//
-//        if (Yii::$app->request->isPost) {
-//            // Загружаем файл
-//            $model->file = UploadedFile::getInstance($model, 'file');
-//            if ($path = $model->upload()) {
-//                // Загружаем конфигурацию из базы данных в виде массива
-//                $config = $model->getConfiguration();
-//                // Компонентом xlsxParser производим парсинг и вычисление
-//                $model->data = Yii::$app->xlsxParser->parse($path, $config,
-//                    500.0);
-//
-//                return $this->render('table', ['model' => $model]);
-//            }
-//        }
-//        return $this->render('calc', ['model' => $model]);
-//    }
 }
