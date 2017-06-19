2629198bd86ea1c6b0e5e508afe709502cfbfeee
Maksim Sukhomlinov <suhomlinov_mp@ekb.sima-land.ru>
2017-06-19 16:49:42 +0500

    Дополняем комментариями функциональные тесты

--- tests/functional/CalcCest.php
+++ tests/functional/CalcCest.php
@@ -1,15 +1,31 @@
 <?php
 
+/**
+ * Функциональные тесты site/calc
+ *
+ * Class CalcCest
+ */
 class CalcCest
 {
+    /**
+     * Устанавливаем site/calc страницей нахохдения до начала выполнения тестов
+     *
+     * @param FunctionalTester $I
+     */
     public function _before(\FunctionalTester $I)
     {
         $I->amOnRoute('site/calc');
     }
 
+    /**
+     * Проверка наличия элементов на странице
+     *
+     * @param FunctionalTester $I
+     */
     public function seeCalcPage(\FunctionalTester $I)
     {
         $I->see('Вычислить', 'h1');
         $I->see('Отправить', 'button');
+        $I->seeElement('input', ['type' => 'file']);
     }
 }
--- tests/functional/IndexCest.php
+++ tests/functional/IndexCest.php
@@ -1,12 +1,27 @@
 <?php
 
+/**
+ * Функциональные тесты главной страницы
+ *
+ * Class IndexCest
+ */
 class IndexCest
 {
+    /**
+     * Устанавливаем главную страницу до начала выполнения тестов
+     *
+     * @param FunctionalTester $I
+     */
     public function _before(\FunctionalTester $I)
     {
         $I->amOnPage(['/']);
     }
 
+    /**
+     * Проверяем наличие элементов на главной странице
+     *
+     * @param FunctionalTester $I
+     */
     public function seeMainPage(\FunctionalTester $I)
     {
         $I->see('Калькулятор работ', 'h1');
@@ -16,12 +31,24 @@ class IndexCest
         $I->see('Конфигурация', 'a');
     }
 
+    /**
+     * Проверяем переход на главную страницу по заголовку в шапке
+     *
+     * @param FunctionalTester $I
+     */
     public function backToMainPage(\FunctionalTester $I)
     {
         $I->click('Калькулятор работ', 'a');
         $I->see('Калькулятор работ', 'h1');
     }
 
+    /**
+     * Проверка перехода на страницу calc.php (вычисление) по ссылке в
+     * навигационной панели и возвращение на главную кликом по заголовку в
+     * шапке
+     *
+     * @param FunctionalTester $I
+     */
     public function goingOnNavToCalcPageAndBack(\FunctionalTester $I)
     {
         $I->amOnPage(['/']);
@@ -32,6 +59,13 @@ class IndexCest
         $I->see('Калькулятор работ', 'h1');
     }
 
+    /**
+     * Проверка перехода на страницу calc.php (вычисление) по клику кнопки
+     * "Перейти к вычислениям" в центре страницы и возвращение на главную
+     * кликом по заголовку в шапке
+     *
+     * @param FunctionalTester $I
+     */
     public function goingOnButtonToCalcPageAndBack(\FunctionalTester $I)
     {
         $I->amOnPage(['/']);
